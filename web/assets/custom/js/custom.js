57afcb506e481ea6ae0e955c40733de9a871dc85
faab-dev <faab@yandex.ru>
2016-06-29 13:28:51 +0400

    Added ngStyle attribute for each column. Added a registration of active filters and/of sorting.

--- web/assets/custom/js/custom.js
+++ web/assets/custom/js/custom.js
@@ -20,7 +20,107 @@
             getData: function(params) {
                 // ajax request to api
                 return Api.get(params.url()).$promise.then(function(data) {
-                    // @TODO: workout serverside (ajaxAction in AppController)
+                    // init an "active"-object by each ajax request
+                    var objectActive = {
+                        country: false,
+                        city: false,
+                        postcode: false,
+                        street: false,
+                        houseNumber: false,
+                        created: false
+                    };
+                    
+                    // checks each URL parameter
+                    angular.forEach(params.url(), function(value, key) {
+                        switch (key) {
+                            case 'filter[country]':
+                                objectActive['country'] = true;
+                                break
+                            case 'filter[city]':
+                                objectActive['city'] = true;
+                                break
+                            case 'filter[postcode]':
+                                objectActive['postcode'] = true;
+                                break
+                            case 'filter[street]':
+                                objectActive['street'] = true;
+                                break
+                            case 'filter[houseNumber]':
+                                objectActive['houseNumber'] = value != '1-149';
+                                break
+                            case 'filter[created]':
+                                objectActive['created'] = true;
+                                break
+                            case 'sorting[country]':
+                                objectActive['country'] = true;
+                                break
+                            case 'sorting[city]':
+                                objectActive['city'] = true;
+                                break
+                            case 'sorting[postcode]':
+                                objectActive['postcode'] = true;
+                                break
+                            case 'sorting[street]':
+                                objectActive['street'] = true;
+                                break
+                            case 'sorting[houseNumber]':
+                                objectActive['houseNumber'] = true;
+                                break
+                            case 'sorting[created]':
+                                objectActive['created'] = true;
+                                break
+                            default:
+                        }
+                    }, objectActive);
+                    // set CSS style on each column where filter (or sorting) does used
+                    angular.forEach(objectActive, function(value, key) {
+                        switch (key) {
+                            case 'country':
+                                if(value){
+                                    $scope.countryStyle={'background-color':'rgba(204,255,255,0.5'};
+                                }else{
+                                    $scope.countryStyle={};
+                                }
+                                break
+                            case 'city':
+                                if(value){
+                                    $scope.cityStyle={'background-color':'rgba(204,255,255,0.5'};
+                                }else{
+                                    $scope.cityStyle={};
+                                }
+                                break
+                            case 'postcode':
+                                if(value){
+                                    $scope.postcodeStyle={'background-color':'rgba(204,255,255,0.5'};
+                                }else{
+                                    $scope.postcodeStyle={};
+                                }
+                                break
+                            case 'street':
+                                if(value){
+                                    $scope.streetStyle={'background-color':'rgba(204,255,255,0.5'};
+                                }else{
+                                    $scope.streetStyle={};
+                                }
+                                break
+                            case 'houseNumber':
+                                if(value){
+                                    $scope.houseNumberStyle={'background-color':'rgba(204,255,255,0.5'};
+                                }else{
+                                    $scope.houseNumberStyle={};
+                                }
+                                break
+                            case 'created':
+                                if(value){
+                                    $scope.createdStyle={'background-color':'rgba(204,255,255,0.5'};
+                                }else{
+                                    $scope.createdStyle={};
+                                }
+                                break
+                            default:
+                        }
+                    }, objectActive);
+                
                     params.total(data.inlineCount); // recal. page nav controls
                     return data.results;
                 });
